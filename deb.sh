0945c2344d414071c2d16e175b62fe7e3f9ee4b2
Softsun <39539616+softsun-cz@users.noreply.github.com>
2022-07-13 22:50:14 +0200

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -3,7 +3,7 @@
 echo -n > /etc/motd
 apt-get -y remove vim-tiny nano
 apt-get update && apt-get upgrade
-apt-get -y install ssh mc net-tools
+apt-get -y install ssh mc net-tools bc
 sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 service ssh restart
 echo "alias ls='ls -la --color=auto'" >> /root/.bashrc
