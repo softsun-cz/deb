cca2532498bb54efea26b7ba087764c5e622bb12
Softsun <39539616+softsun-cz@users.noreply.github.com>
2022-12-24 21:19:06 +0100

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -7,6 +7,7 @@ apt -y upgrade
 apt-get -y install ssh mc net-tools bc
 echo "alias ls='ls -la --color=auto --group-directories-first'" >> /root/.bashrc
 sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/GRUB_CMDLINE_LINUX_DEFAULT="quiet consoleblank=0/g' /etc/default/grub
 sed -i -e 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
 update-grub
 reboot
