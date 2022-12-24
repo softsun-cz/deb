184da5a409c4b5710ea88b5967995fa7034fb6e6
Softsun <39539616+softsun-cz@users.noreply.github.com>
2022-12-24 21:15:35 +0100

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -5,6 +5,6 @@ apt-get -y remove vim-tiny nano
 apt-get update && apt-get upgrade
 apt-get -y install ssh mc net-tools bc
 sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-service ssh restart
+sed -i -e 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
 echo "alias ls='ls -la --color=auto --group-directories-first'" >> /root/.bashrc
 reboot
